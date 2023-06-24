ab6a3c564aa68d1ce6d9f92fd0128ddfc1433dae
Stone Chen <chen.stone@outlook.com>
2023-06-23 21:02:38 -0400

    update math.h to remove stdint etc

--- median_filter/mf1/math.h
+++ median_filter/mf1/math.h
@@ -1,11 +1,9 @@
 #ifndef MATH_H
 #define MATH_H
 
-#include <stdint.h>
-#include <float.h>
 
-typedef uint32_t u32;
-typedef int32_t s32;
+typedef unsigned int u32;
+typedef int s32;
 typedef float f32;
 typedef double f64;
 
