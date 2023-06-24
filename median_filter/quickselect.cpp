051536dc265fc179e077e955f38e86dcd886e467
Stone Chen <chen.stone@outlook.com>
2023-06-23 20:12:13 -0400

    quick select bug fix

--- median_filter/quickselect.cpp
+++ median_filter/quickselect.cpp
@@ -8,34 +8,32 @@ void swap(f32 *a, f32 *b)
     *b = temp;
 }
 
+// f32 *Partition(f32 *arr, u32 count, u32 index)
+// {
+
+// }
+
 f32 *Partition(f32 *arr, u32 count, u32 index)
 {
     if(count == 1 & index == 1) return(arr);
-    s32 pivotIdx = rand() % count;
+    s32 pivotIdx = (rand() + rand()) % count;
     f32 pivot = arr[pivotIdx];
 
     u32 frontIdx = 0;
     u32 backIdx = count - 1;
-
-    //  3, 4, 1, 5, 2
-    //  2, 4, 1, 5, 3
-    //  2, 3, 1, 5, 4
-
-    // 3 , 5, 4, 2, 1
-    // 1, 5, 4 , 2, 3
-    // 1, 2, 4, 5, 3
     
     while(frontIdx < backIdx)
     {
         while(arr[frontIdx] < pivot) {
-            ++frontIdx;  //if we exit, then frontIdx points at > pivot
+            ++frontIdx;  //if we exit, then frontIdx points at >= pivot
         }
-        while(arr[backIdx] >= pivot) {
-            --backIdx; //exit, points at < pivot
+        while(arr[backIdx] > pivot) {
+            --backIdx; //exit, points at <= pivot
         }
         if(frontIdx < backIdx)
         {
-            swap(arr + frontIdx++, arr + backIdx--);
+            swap(arr + frontIdx++, arr + backIdx);
+            //left half will be pivot or less, right pivot or greater
         }
     }
 
@@ -64,6 +62,9 @@ int main()
 {
     f32 test[] = {0.5, 6, 4.1, 1, 0, 0, 3.2};
     f32 *ptr = Partition(test, 7, 3 + 1);
+    f32 *ptr2 = Partition(ptr+1, 7 - 4, 1);
+    f32 test2[] = {0,0,0,1,0,1};
+    f32 *ptr3 = Partition(test2, 6, 4);
 
 }
 
