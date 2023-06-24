68cb6915ab8d899058cf52679c5c7ccaed443b21
Stone Chen <chen.stone@outlook.com>
2023-06-23 20:50:39 -0400

    fix up boundaries

--- median_filter/mf1/mf.cc
+++ median_filter/mf1/mf.cc
@@ -11,40 +11,46 @@ This is the function you need to implement. Quick reference:
 #include "math.h"
 #include "quickselect.cpp"
 
-void mf(int ny, int nx, int hy, int hx, const float *in, float *out)
+void mf(int ny, int nx, int hy, int hx, const float* in, float* out)
 {
-  u32 CenterX = 0, CenterY = 0;
-
-  s32 MinX = CenterX - hx; if(MinX < 0) MinX = 0;
-  s32 MinY = CenterY - hy; if(MinY < 0) MinY = 0;
-  s32 MaxX = CenterX + hx; if(MaxX > nx) MaxX = nx; 
-  s32 MaxY = CenterY + hy; if(MaxY > ny) MaxY = ny;
-
-  f32 *arr = (f32 *)malloc((2 * hy + 1) * (2 * hx + 1) * sizeof(f32));
-
-  // f32 *ptr = arr;
-  u32 total = 0;
-  for(u32 x = MinX; x <= MaxX; ++x)
+  f32* arr = (f32*)malloc((2 * hy + 2) * (2 * hx + 2) * sizeof(f32));
+  for (s32 CenterY = 0; CenterY < ny; ++CenterY)
   {
-    for(u32 y = MinY; y <= MaxY; ++y)
-    {
-      arr[total++] = in[nx*y + x];
+  for (s32 CenterX = 0; CenterX < nx; ++CenterX)
+  {
+      // u32 CenterX = 0, CenterY = 0;
+
+      s32 MinX = CenterX - hx; if (MinX < 0) MinX = 0;
+      s32 MinY = CenterY - hy; if (MinY < 0) MinY = 0;
+      s32 MaxX = CenterX + hx; if (MaxX > nx - 1) MaxX = nx - 1;
+      s32 MaxY = CenterY + hy; if (MaxY > ny - 1) MaxY = ny - 1;
+
+
+      // f32 *ptr = arr;
+      u32 total = 0;
+      for (s32 y = MinY; y <= MaxY; ++y)
+      {
+      for (s32 x = MinX; x <= MaxX; ++x)
+        {
+          arr[total++] = in[nx * y + x];
+        }
+      }
+
+      f32 median = 0;
+      if (total % 2 == 1)
+      {
+        median = *QuickSelect(arr, total, total / 2 + 1);
+      }
+      else
+      {
+        median = *QuickSelect(arr, total, total / 2);
+        median += *QuickSelect(arr, total, total / 2 + 1);
+        median /= 2;
+      }
+
+      out[nx * CenterY + CenterX] = median;
+
     }
   }
-  
-  f32 median = 0;
-  if(total % 2 == 1)
-  {
-    median = *QuickSelect(arr, total, total/2 + 1);
-  }
-  else
-  {
-    median  = *QuickSelect(arr, total, total/2);
-    median += *QuickSelect(arr, total, total/2 + 1);
-    median /=2;
-  }
-
-  out[nx * CenterY + CenterX] = median;
-
-
+  free(arr);
 }
