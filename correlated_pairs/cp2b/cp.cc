847c5cd482a719b550c9b77b18792fdab87417b1
Stone Chen <chen.stonechen@gmail.com>
2023-06-25 10:27:36 -0400

    normalization code

--- correlated_pairs/cp2b/cp.cc
+++ correlated_pairs/cp2b/cp.cc
@@ -1,12 +1,52 @@
 /*
-This is the function you need to implement. Quick reference:
-- input rows: 0 <= y < ny
-- input columns: 0 <= x < nx
-- element at row y and column x is stored in data[x + y*nx]
-- correlation between rows i and row j has to be stored in result[i + j*ny]
-- only parts with 0 <= j <= i < ny need to be filled
+    Multicore via OMP
+
 */
+
+
+
+
+#include <stdlib.h>
+#include <cmath>
+
+typedef unsigned int u32;
+typedef int s32;
+typedef float f32;
+typedef double f64;
 void correlate(int ny, int nx, const float *data, float *result) 
 {
+    s32 VecDim = 8;
+    s32 VecCount = (nx + VecDim - 1) / VecDim;
+    s32 PaddedX = VecDim * VecCount;
+
+    f64 *NormData = (f64 *)malloc(ny * PaddedX * sizeof(f64));
+    for(s32 Row = 0; Row < ny; ++Row)
+    {
+        f64 Sum = 0;
+        for(s32 Col = 0; Col < PaddedX; ++Col)
+        {
+            f64 val = (Col < nx) ? data[nx*Row + Col] : 0;
+            NormData[PaddedX*Row + Col] = val;
+            Sum += val;
+        }
+
+        f64 Mean = Sum/nx;
+        f64 SumSq = 0;
+        for(s32 Col = 0; Col < nx; ++Col)
+        {
+            f64 val = NormData[PaddedX*Row + Col] - Mean;
+            NormData[PaddedX*Row + Col] = val;
+            SumSq += val*val;
+        }
+
+        f64 InvStdY = 1/sqrt(SumSq);
+        for(s32 Col = 0; Col < nx; ++Col)
+        {
+            NormData[PaddedX*Row + Col] *= InvStdY;
+        }
+    }
+
     
+
+
 }
