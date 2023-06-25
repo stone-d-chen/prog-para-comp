d5ccd38f090fb061e4c0b495b42765c3b5f02b97
Stone Chen <chen.stonechen@gmail.com>
2023-06-25 10:36:07 -0400

    just OMP fors

--- correlated_pairs/cp2b/cp.cc
+++ correlated_pairs/cp2b/cp.cc
@@ -15,11 +15,13 @@ typedef float f32;
 typedef double f64;
 void correlate(int ny, int nx, const float *data, float *result) 
 {
-    s32 VecDim = 8;
+    const s32 VecDim = 8;
     s32 VecCount = (nx + VecDim - 1) / VecDim;
     s32 PaddedX = VecDim * VecCount;
 
     f64 *NormData = (f64 *)malloc(ny * PaddedX * sizeof(f64));
+
+    #pragma omp parallel for
     for(s32 Row = 0; Row < ny; ++Row)
     {
         f64 Sum = 0;
@@ -46,7 +48,28 @@ void correlate(int ny, int nx, const float *data, float *result)
         }
     }
 
-    
 
-
+    // the output dim
+    #pragma omp parallel for
+    for(s32 Row = 0; Row < ny; ++Row)
+    {
+        for(s32 Col =  Row; Col < ny; ++Col)
+        {
+            f64 DotProds[VecDim] = {};
+            for(s32 VecIdx = 0; VecIdx < VecCount; ++VecIdx)
+            {
+                for(s32 ItemIdx = 0; ItemIdx < VecDim; ++ItemIdx)
+                {
+                    DotProds[ItemIdx] += NormData[PaddedX*Row + VecIdx*VecDim + ItemIdx]*NormData[PaddedX*Col + VecIdx*VecDim + ItemIdx];
+                }
+            }
+            f64 FinalSum = 0;
+            for(s32 ItemIdx = 0; ItemIdx < VecDim; ++ItemIdx)
+            {
+                FinalSum += DotProds[ItemIdx];
+            }
+            result[ny*Row + Col] = FinalSum;
+        }
+    }
+    free(NormData);
 }
