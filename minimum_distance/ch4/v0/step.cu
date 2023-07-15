064c709d21454ec8a437a8c2b2d1475bdd000628
Stone Chen <chen.stonechen@gmail.com>
2023-07-15 12:45:21 -0400

    more review

--- minimum_distance/ch4/v0/step.cu
+++ minimum_distance/ch4/v0/step.cu
@@ -1,19 +1,70 @@
 void step(float* r, const float* d, int n) {
-  
+  for(int i = 0; i < n; ++i)
+  {
+    for(int j = 0; j < n; ++j)
+    {
+        float v = 1e100;
+        for(int k = 0; k < n; ++k)
+        {
+            float x = d[n*i + k];
+            float y = d[n*k + j];
+            v = min(v, x+ y);
+        }
+
+        r[n*i + j] = v;
+    }
+  }
   
 }
 
-__global__ void mykernel(float* r, const float* d, int n) {
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    int j = threadIdx.y + blockIdx.y * blockDim.y;
-    if (i >= n || j >= n)
-        return;
-    float v = HUGE_VALF;
-    for (int k = 0; k < n; ++k) {
+/*
+    n x n x n units of work;
+    we'll let 1 thread do 1 x 1 x n of work
+    We'll create dimBlocks(16, 16);
+
+    we'll create dimGrid(n/16, n/16);
+
+*/
+
+
+
+__global__
+void mykernel(float *r, float *d, int n)
+{
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+    int j = threadIdx.y + blockDim.y * blockIdx.y;
+    if(i >= n || j >= n) return;
+
+    int v = HUGE_VALF;
+
+    for(k = 0; k < n; ++k)
+    {
         float x = d[n*i + k];
         float y = d[n*k + j];
-        float z = x + y;
-        v = min(v, z);
+        v = min(v, x + y);
     }
     r[n*i + j] = v;
+}
+
+void step(float *r, const float *d, int n)
+{
+    float *dGPU = NULL;
+    cudaMalloc((void **)&dGPU, sizeof(float) * n * n );
+
+    float *rGPU = NULL;
+    cudaMalloc((void **) &rGPU, sizeof(float) * n * n);
+
+    cudaMemcpy(dGPU, d, sizeof(float) * n * n, cudaMemcpyHostToDevice);
+
+
+    dim3 dimBlock(16, 16);
+    dim3 dimGrid(divup(n, dimBlock.x), divup(n, dimBlock.y));
+
+    mykernel<<<dimGrid, dimBlock>>>(dGPU, rGPU, n);
+
+    cudaMemcpy(r, rGPU, sizeof(float) * n * n, cudaMemcpyDeviceToHost);
+
+
+
+
 }
